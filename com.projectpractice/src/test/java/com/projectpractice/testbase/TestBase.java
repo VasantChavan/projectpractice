d256cf9f2c95985954ebde2f702e97c0d2444f35
Vasant Chavan <vasant.chavan223@gmail.com>
2024-10-11 10:15:05 +0530

    Updated ConfigReasder,TestBase and LoginTest class, updated with pom.xml

--- com.projectpractice/src/test/java/com/projectpractice/testbase/TestBase.java
+++ com.projectpractice/src/test/java/com/projectpractice/testbase/TestBase.java
@@ -1,17 +1,11 @@
 package com.projectpractice.testbase;
 
-//import java.io.File;
-//import java.io.IOException;
 import java.time.Duration;
 
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.io.FileHandler;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
@@ -20,43 +14,29 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import com.projectpractice.pageobjects.LoginPage;
-
-//import com.inetbanking_hybridframework.pageobjects.HomePage;
-//import com.inetbanking_hybridframework.pageobjects.LoginPage;
-//import com.inetbanking_hybridframework.utility.ConfigDataProvider;
-//import com.inetbanking_hybridframework.utility.ExcelDataProvider;
-//import com.inetbanking_hybridframework.utility.Helper;
+import com.projectpractice.utility.ConfigReader;
 
 public class TestBase {
-	//public static LoginPage lp;
-	//public static HomePage hm;
-//public static ExcelDataProvider exceldataprovider;
+	
+	public static LoginPage lp;	
 	public static WebDriver driver;
-
-	//public static ConfigDataProvider configdatapr;
-
+	
+	public static ConfigReader configReader;
+	
 	@BeforeSuite
-	public void init() {
-		//configdatapr = new ConfigDataProvider("config");
-		//exceldataprovider= new ExcelDataProvider("inetBankingTestData");
+	public void init()
+	{
+		configReader=new ConfigReader("./Config/prop.properties");
 	}
 
 	@BeforeClass
 	public void initialisePageObjects() {
-		lp = new LoginPage(driver);
-
-		//lp.setUserName(configdatapr.getUserName());
-		//lp.setpassword(configdatapr.getPassword());
-		
-		//lp.setUserName(exceldataprovider.getStringCellData("Login", 1, 0));
-		//lp.setpassword(exceldataprovider.getStringCellData("Login", 1, 1));
-
-		//hm = lp.clickonLoginBtn();
-
+		lp = new LoginPage(driver);		
 	}
 
-	@BeforeTest
+	
 	@Parameters("browserName")
+	@BeforeTest
 
 	public void setUp(@Optional("Chrome") String browserName) {
 		if (browserName.equals("Chrome")) {
@@ -70,10 +50,9 @@ public class TestBase {
 			driver = new EdgeDriver();
 
 		}
-		//driver.get(configdatapr.getAppURL());
+		
 		driver.manage().window().maximize();
-
-		// driver.get("https://demo.guru99.com/v4/");
+		driver.get("https://demo.guru99.com/v4/");
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
@@ -83,14 +62,5 @@ public class TestBase {
 
 		driver.quit();
 	}
-
-	//public static String captureScreenshot(String tname) throws WebDriverException, IOException {
-		//String screenshotPath = System.getProperty("user.dir") + "//Screenshots//" + tname + " " + Helper.getTimeStamp()+ ".png";
-
-		//FileHandler.copy(((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE), new File(screenshotPath));
-
-		//return screenshotPath;
-	//}
-
 
 }
