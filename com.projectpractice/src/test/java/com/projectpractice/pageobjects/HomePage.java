1938996fd6f51f8d20bbba1dbd3cf5ade624fdbe
HP <HP@LAPTOP-UM9ID17S>
2024-10-12 09:57:33 +0530

    updated HomePage

--- com.projectpractice/src/test/java/com/projectpractice/pageobjects/HomePage.java
+++ com.projectpractice/src/test/java/com/projectpractice/pageobjects/HomePage.java
@@ -7,7 +7,7 @@ public class HomePage {
 	WebDriver driver;
 	
 	
-	public void HomePage(WebDriver driver)
+	public  HomePage(WebDriver driver)
 	{
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
