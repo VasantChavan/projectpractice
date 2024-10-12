3328cefa0bad6714fda6e03dce5e93044c9988f9
HP <HP@LAPTOP-UM9ID17S>
2024-10-12 09:52:19 +0530

    Hi Team,updated HomePage.java with constructor

--- com.projectpractice/src/test/java/com/projectpractice/pageobjects/HomePage.java
+++ com.projectpractice/src/test/java/com/projectpractice/pageobjects/HomePage.java
@@ -1,5 +1,18 @@
 package com.projectpractice.pageobjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+	WebDriver driver;
+	
+	
+	public void HomePage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+		
+		
+	}
 
 }
