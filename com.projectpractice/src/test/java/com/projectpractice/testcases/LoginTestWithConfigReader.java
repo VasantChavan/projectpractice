bd1a4f369b66ff546154be11aa2418213ad0084a
nikita <nikita.s9298@gmail.com>
2024-10-17 18:03:00 +0530

    added assertion for successful login

--- com.projectpractice/src/test/java/com/projectpractice/testcases/LoginTestWithConfigReader.java
+++ com.projectpractice/src/test/java/com/projectpractice/testcases/LoginTestWithConfigReader.java
@@ -1,5 +1,7 @@
 package com.projectpractice.testcases;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.projectpractice.testbase.TestBase;
@@ -10,6 +12,11 @@ public class LoginTestWithConfigReader extends TestBase {
 		lp.setUserName(configReader.getUserId());
 		lp.setpassword(configReader.getPassword());
 		lp.clickOnLoginBtn();
+		
+		//Assert page title after login
+		Assert.assertEquals(driver.getTitle(), "Guru99 Bank Manager HomePage");
+		//Assert element after login
+		Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]")).isDisplayed());
 		
 	}
 
